860dbd1021d7ee636a7e336a30943bd4d2f03537
Nathan da Silveira Uflacker <nathanuflacker@hotmail.com>
2025-08-18 11:21:14 -0300

    Estruturas Relacionais/Condicionais

--- Aula2/Program.cs
+++ Aula2/Program.cs
@@ -6,7 +6,29 @@ namespace Aula2
     {
         static void Main(string[] args)
         {
-            Console.WriteLine("hello world!");
+            int n1, n2;
+            int r;
+
+            Console.WriteLine("Bem-vindo, digite 2 números: ");
+            n1 = int.Parse(Console.ReadLine());
+
+            Console.WriteLine("Digite o segundo número: ");
+            n2 = int.Parse(Console.ReadLine());
+
+            r = n1 + n2;
+
+            if (r == 0)
+            {
+                Console.WriteLine("A soma do número " + n1 + " com " + n2 + " é igual que 0.");
+            }                                                  
+            if (r > 0)                                         
+            {                                                  
+                Console.WriteLine("A soma do número " + n1 + " com " + n2 + " é maior que 0.");
+            }
+            if (r < 0)
+            {
+                Console.WriteLine("A soma do número" + n1 + " com " + n2 + " é maior que 0.");
+            }
         }
     }
 }
