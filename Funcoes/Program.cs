fa50f814dba7772c38fea077be65de0a82fd8665
Nathan da Silveira Uflacker <nathanuflacker@hotmail.com>
2025-08-25 19:36:55 -0300

    Atualizando Atividade
    Inserindo comentários e finalizando alterações.

--- Funcoes/Program.cs
+++ Funcoes/Program.cs
@@ -4,8 +4,10 @@ namespace Funcoes
 {
     class Program
     {
-
-        static void  Print(string value)
+        //<summary>
+        // Criando duas opções de Print para retornar tanto string quanto double
+        //</summary>
+        static void Print(string value)
         {
             Console.WriteLine(value);
         }
@@ -15,14 +17,21 @@ namespace Funcoes
             Console.WriteLine(value);
         }
 
-        static double CalcularArea(double raio) {
-        double r = 0;
-            r = Math.Pow(raio, 2) * Math.PI;
 
-            return r;
-        }
+        //static double CalcularArea(double raio)
+        //{
+        //    double r = 0;
+        //    r = Math.Pow(raio, 2) * Math.PI;
 
-        static double CalcularRetangulo(double b, double h) { 
+        //    return r;
+        //}
+
+        //<summary>
+        // Criando funções de calcular area. Primeiro criamos a opção para calcular Retangulo, logo em seguida Circulo e finalmente o triângulo. 
+        //</summary>
+
+        static double CalcularRetangulo(double b, double h)
+        {
             double area = b * h;
             return area;
         }
@@ -33,9 +42,10 @@ namespace Funcoes
             return area;
         }
 
-        static void CalcularTriangulo(double b, double h) {
+        static double CalcularTriangulo(double b, double h)
+        {
             double area = (b * h) / 2;
-            Print("A area do triangulo é: " + area);
+            return area;
         }
 
 
@@ -46,11 +56,16 @@ namespace Funcoes
             //double resp = CalcularArea(raio);
             //Print("A area do circulo é:" + resp);
 
+            //<summary>
+            // Dentro da execução criamos um programa que cumprimenta nosso usuário, deixando claro a finalidade do sistema e apresentando opções de calculo através de números, para facilitar a leitura pelo programa.
+            // Após isso, criamos uma estrutura condicional para cada opção, onde o usuário insere a opção de calculo desejada, logo em seguida o programa lê a resposta e entra na estrutura condicional correta.
+            //</summary>
+
             Print("Olá! Seja bem vindo ao programa de calculos de areas!");
             Print("--------------------------------");
             Print("Insira a opção que deseja calcular:");
             Print("--------------------------------");
-            Print("1 - Area do circulo \n"+
+            Print("1 - Area do circulo \n" +
                 "2 - Area do retangulo \n" +
                 "3 - Area do triangulo");
             string resposta_usuario = Console.ReadLine();
@@ -68,6 +83,7 @@ namespace Funcoes
                 Print("Insira o valor da altura: ");
                 double altura_retangulo = double.Parse(Console.ReadLine());
                 CalcularRetangulo(base_retangulo, altura_retangulo);
+                Print("A area do retangulo é: " + CalcularRetangulo(base_retangulo, altura_retangulo));
             }
             else if (resposta_usuario == "3")
             {
@@ -76,9 +92,12 @@ namespace Funcoes
                 Print("Insira o valor da altura: ");
                 double altura_triangulo = double.Parse(Console.ReadLine());
                 CalcularTriangulo(base_triangulo, altura_triangulo);
+                Print("O valor da área é de: ");
+                Print(CalcularTriangulo(base_triangulo, altura_triangulo)); // Mostrando que o Print funciona com double
             }
-            else {
-                Print("Por favor, insira uma opção válida!" );
+            else
+            {
+                Print("Por favor, insira uma opção válida!"); // Caso o usuário digite algo fora da programação esperada.
             }
         }
     }
