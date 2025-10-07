b7c35ba7483ab19f6e63b6efa6bd96274e225329
Nathan da Silveira Uflacker <nathanuflacker@hotmail.com>
2025-10-06 22:05:15 -0300

    Parte2Prova

---
+++ prova_pt2/Program.cs
@@ -0,0 +1,36 @@
+﻿using System;
+
+namespace Programamtmassa
+{
+    public class Livro
+    {
+
+        public string Titulo;
+        public string Autor;
+        public int AnoPublicacao;
+
+        public Livro(string titulo, string autor, int AnoPublicacao)
+        {
+            this.Titulo = titulo;
+            this.Autor = autor;
+            this.AnoPublicacao = AnoPublicacao;
+        }
+
+        public void ExibirInformacoes()
+        {
+            Console.WriteLine($"{Titulo} – {Autor} ({AnoPublicacao})");
+        }
+    }
+
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Livro livro1 = new Livro("Reuri Pote -", "J.K Rowling", 1954);
+
+            livro1.ExibirInformacoes();
+
+            // O que esperamos de saída: Reuri Pote - J.K Rowling (1954)
+        }
+    }
+}
